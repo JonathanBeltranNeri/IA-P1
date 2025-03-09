2ffb92074515dd93ffb1b90c541d945ea1b0929c
JonaBN <a22310188@ceti.mx>
2025-03-09 15:27:22 -0600

    Capitulo 6
    Se realiza una suma de num1, num2, 40 y 4, y el resultado se imprime usando el signo +.
    
    Se realiza una resta de num1, num2, 40 y 4, y el resultado se imprime usando el signo -.
    
    Se realiza una multiplicación de num1, num2 y 1.8915, y el resultado se imprime usando el signo *.
    
    Se realiza una división secuencial de 5000 entre 230 y luego entre 2.1739, y el resultado se imprime usando el signo /.
    
    Se realiza una operación combinada que involucra división, suma y resta, y se imprime el resultado.

---
+++ 006_Practica1_Suma_Resta_Multiplicacion_Division.py
@@ -0,0 +1,25 @@
+## JonathanBN
+# La suma, resta, multiplicación y división en Python
+
+num1=20
+num2=23
+
+# Realiza la suma de num1, num2, 40 y 4, y almacena el resultado en 'suma'.
+suma=num1+num2+40+4
+print("Suma:",suma)# Imprime el resultado de la suma.
+
+# Realiza la resta de num1, num2, 40 y 4, y almacena el resultado en 'resta'.
+resta=num1-num2-40-4
+print("Resta:",resta)# Imprime el resultado de la resta.
+
+# Realiza la multiplicación de num1, num2 y 1.8915, y almacena el resultado en 'multiplicacion'.
+multiplicacion=num1*num2*1.8915
+print("Multiplicacion",multiplicacion)# Imprime el resultado de la multiplicación.
+
+# Realiza la división de 5000 entre 230 y luego entre 2.1739, y almacena el resultado en 'division'.
+division = 5000 / 230 / 2.1739
+print("Division",division)# Imprime el resultado de la división.
+
+# Realiza una operación combinada de división, suma y resta, y almacena el resultado en 'operacion'.
+operacion = 10 / 5 + 15 - 17 / 1
+print("El resultado de la operacion:",operacion)# Imprime el resultado de la operación combinada.

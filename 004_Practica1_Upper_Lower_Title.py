60458f769e9c010d77f8fca62ef5283c4f4be97d
JonaBN <a22310188@ceti.mx>
2025-03-09 15:04:38 -0600

    Capitulo 4
    Se usa el método title() para convertir la primera letra de cada palabra en mayúscula en la variable nom.
    
    Se usa el método lower() para convertir todo el texto de mensaje1 a minúsculas y se imprime.
    
    Se usa el método upper() para convertir todo el texto de mensaje2 a mayúsculas y se imprime.

---
+++ 004_Practica1_Upper_Lower_Title.py
@@ -0,0 +1,20 @@
+## JonathanBN
+# Los métodos upper(), lower() y title de Python
+
+# Convierte la primera letra de cada palabra en mayúscula usando title().
+nom="jonathan beltran neri"
+nom=nom.title()
+# Imprime el nombre con la primera letra de cada palabra en mayúscula.
+print(nom)
+
+# Convierte todo el texto a minúsculas usando lower().
+mensaje1="Esta Es Una Frase Para Ser Formateada."
+mensaje1=mensaje1.lower()
+# Imprime el texto en minúsculas.
+print(mensaje1)
+
+# Convierte todo el texto a mayúsculas usando upper().
+mensaje2="Esta Es Una Frase Para Ser Formateada."
+mensaje2=mensaje2.upper()
+# Imprime el texto en mayúsculas.
+print(mensaje2)

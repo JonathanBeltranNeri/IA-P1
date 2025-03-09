6f1cb3b798afbe105586b8fd32ae931dda36cd5c
JonaBN <a22310188@ceti.mx>
2025-03-09 15:52:13 -0600

    Create 009_Practica1_Listas.py
    Se crea la lista colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"].
    
       Se imprime el color que está en la posición 3, que es "amarillo".
    
    Se imprime el color en la posición 0 (rojo) y en la posición 7 (rosa) de la lista colores.
    
    Se crea la lista lista = ["tres", "dos", "cinco", "cuatro", "uno"] y luego se imprime.

---
+++ 009_Practica1_Listas.py
@@ -0,0 +1,15 @@
+## JonathanBN
+# Cómo crear listas y utilizarlas en Python
+
+#De la siguiente lista, ¿qué color está en la posición 3?
+# Se define una lista con colores.
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(colores[3])# Imprime el color que está en la posición 3 de la lista 'colores'.
+
+
+#¿En qué posición se encuentra el color 'rojo'? ¿y el 'rosa'?
+print(colores[0],",",colores[7])# Imprime los colores en las posiciones 0 y 7 de la lista 'colores'.
+
+# Se define una lista 
+lista=["tres","dos","cinco","cuatro","uno"]
+print(lista)# Imprimimos la lista 

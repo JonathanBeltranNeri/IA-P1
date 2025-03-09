004966ba7b5841e86f53cb9db487eca3e3f2324e
JonaBN <a22310188@ceti.mx>
2025-03-09 14:54:22 -0600

    Capitulo 3
    Se concatenan dos cadenas de texto ("Hola esto es" y " un codigo en python") y se almacenan en var1.
    
    Luego, se imprime var1.
    Se concatenan las variables var2 y var3, y el resultado se almacena en var4, que luego se imprime.
    
    Se concatenan var2 y var3 directamente dentro de un print().
    
    Se almacenan el nombre y apellidos en las variables nom y ape, respectivamente, luego se concatenan en la variable final y se imprime.
    
    Se concatenan dos números representados como strings (num1 y num2), sin sumarlos, y el resultado se imprime.

---
+++ 003_Practica1_Concatenacion.py
@@ -0,0 +1,32 @@
+## JonathanBN
+# ¿Qué es la concatenación en Python?
+
+# Concatenación de dos strings en una variable.
+var1="Hola esto es"+" un codigo en python"
+# Imprime el contenido de var1.
+print(var1)
+
+# Concatenación de dos variables con strings en una tercera variable.
+var2="Hola mi nombre es:"
+var3="Jonathan"
+var4=var2+var3
+# Imprime el resultado de concatenar var2 y var3.
+print(var4)
+
+# Concatenación de dos variables con strings directamente en un print().
+print(var2+var3)
+
+# Escribe el nombre y apellidos en variables y concaténalos en otra variable.
+nom="Jonathan"
+ape=" Beltran Neri"
+final=nom+ape
+# Imprime el resultado de concatenar el nombre y los apellidos.
+print(final)
+
+
+# Concatenación de dos números como strings sin sumarlos.
+num1="103"
+num2="103"
+num3=num1+num2
+# Imprime el resultado de concatenar num1 y num2.
+print(num3)

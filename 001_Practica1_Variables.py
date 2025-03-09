5ddff4aa9d9c7a5c693b622f3b9fa76b0c01108d
JonaBN <a22310188@ceti.mx>
2025-03-09 14:34:44 -0600

    Capitulo 1
    El código utiliza la función print() para mostrar en consola el mensaje "Hello world". Luego, define dos variables (var1 y var2) con valores numéricos, suma esos valores y almacena el resultado en la variable var3. Finalmente, usa nuevamente print() para mostrar el resultado de la suma.

---
+++ 001_Practica1_Variables.py
@@ -0,0 +1,14 @@
+## JonathanBN
+#Variables
+
+# Se define una variable 'mensaje' y se le asigna la cadena de texto "Hello world".
+mensaje=('Hello world')
+# Se imprime el contenido de la variable 'mensaje'.
+print(mensaje)
+# Se declaran dos variables numéricas, 'var1' y 'var2', con valores 120 y 90 respectivamente.
+var1=120
+var2=90
+# Se define 'var3' como la suma de 'var1' y 'var2'.
+var3=var1+var2
+# Se imprime el valor de 'var3', que será el resultado de la suma de 120 + 90.
+print(var3)

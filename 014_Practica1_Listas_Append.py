531d9f9b64e53c1e9d4cc93f1f58e2d0b92da376
JonaBN <a22310188@ceti.mx>
2025-03-09 16:36:21 -0600

    Capitulo 14
    Se define la lista colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'].
    
    Se utilizan los métodos append() para agregar nuevos colores a la lista colores:
    
        colores.append("fuxia") agrega el color "fuxia" al final de la lista.
        colores.append("celeste") agrega el color "celeste" al final de la lista.
    
    Se imprime la lista colores, que ahora incluye los nuevos colores "fuxia" y "celeste" al final.

---
+++ 014_Practica1_Listas_Append.py
@@ -0,0 +1,11 @@
+## JonathanBN 
+# Insertar elementos en listas Python
+
+# Se define una lista con colores.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Se agregan nuevos colores a la lista usando el método append().
+lista=[colores.append("fuxia"),colores.append("celeste")]
+
+# Imprime la lista 'colores' después de agregar los nuevos colores.
+print(colores)

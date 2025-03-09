199fb531947f4af85aff1bac04a34ae80723b301
JonaBN <a22310188@ceti.mx>
2025-03-09 16:15:13 -0600

    Capitulo 12
    Se crea la lista colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'].
    
        Usamos lista[] para definir la lista con los colores iniciales.
    
    Se utilizan los métodos remove() dentro de la lista lista para eliminar los elementos "amarillo" y "rojo" de la lista colores:
    
        colores.remove("amarillo") elimina el color "amarillo" de la lista.
        colores.remove("rojo") elimina el color "rojo" de la lista.
    
    Finalmente, print(colores) se utiliza para imprimir la lista colores después de las eliminaciones. El resultado es que los colores "amarillo" y "rojo" ya no están en la lista.

---
+++ 012_Practica1_Lista_Remove.py
@@ -0,0 +1,9 @@
+## JonathanBN
+# Eliminar elementos en listas Python con remove()
+
+# Se define una lista con colores.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+# Se eliminan los colores "amarillo" y "rojo" de la lista 'colores' usando el método remove().
+lista=[colores.remove("amarillo"),colores.remove("rojo")]
+# Imprime la lista 'colores' después de eliminar los elementos.
+print(colores)

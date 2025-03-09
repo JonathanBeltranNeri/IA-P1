2fd9bdafcabe489f44714d1eb0f4b3faebed79f1
JonaBN <a22310188@ceti.mx>
2025-03-09 16:07:56 -0600

    Capitulo 11
    Se crea la lista colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'].
    
    Se eliminan los elementos en las siguientes posiciones con del :
    
        del colores[4] elimina "marrón".
        del colores[1] elimina "azul".
        del colores[-4] elimina "rosa".
        del colores[-3] elimina "blanco".
    
    Se imprime la lista colores después de realizar las eliminaciones con print().

---
+++ 011_Practica1_Listas_del.py
@@ -0,0 +1,12 @@
+## JonathanBN
+# Eliminar elementos de listas en Python
+
+# Se define una lista con colores.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+del colores[4] # Elimina 'marrón' (en la posición 4)
+del colores[1] # Elimina 'azul' (en la posición 1)
+del colores[-4]# Elimina 'rosa' (en la posición -4)
+del colores[-3]# Elimina 'blanco' (en la posición -3)
+
+# Imprime la lista 'colores' después de eliminar los elementos.
+print(colores)

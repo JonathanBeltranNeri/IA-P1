e785deba5cc9490e9adc7dca835868b37e88b22e
JonaBN <a22310188@ceti.mx>
2025-03-09 15:31:23 -0600

    Capitulo 7
    El código realiza la multiplicación de 2 por sí mismo diez veces y luego imprime el resultado, que es 2^10 o 1024.

---
+++ 007_Practica1_Exponentes.py
@@ -0,0 +1,6 @@
+## JonathanBN
+# Cómo calcular exponentes en Python
+
+# Realiza la multiplicación repetida de 2 diez veces y almacena el resultado en 'num'.
+num=2*2*2*2*2*2*2*2*2*2
+print(num)# Imprime el resultado de la multiplicación.

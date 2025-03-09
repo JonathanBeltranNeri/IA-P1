bb49a2cd30d95cfcdf6678e733ec471e6cdad38a
JonaBN <a22310188@ceti.mx>
2025-03-09 16:43:36 -0600

    Capitulo 15
    Se define la lista colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'].
    
        Esta lista contiene una serie de colores iniciales.
    
    Se utilizan los métodos insert() para agregar elementos en posiciones específicas de la lista colores:
    
        colores.insert(6, "magenta") inserta el color "magenta" en la posición 6 (después de "lila").
        colores.insert(-1, "turquesa") inserta el color "turquesa" en la penúltima posición de la lista (antes de "naranja").
    
    Se imprime la lista colores después de la inserción, mostrando los nuevos colores en las posiciones especificadas.

---
+++ 015_Practica1_Listas_Insert.py
@@ -0,0 +1,11 @@
+## JonathanBN
+# Insertar elementos en listas Python
+
+# Se define una lista con colores.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Se insertan nuevos colores en posiciones específicas de la lista usando el método insert().
+lista=[colores.insert(6,"magenta"),colores.insert(-1,"turquesa")]
+
+# Imprime la lista 'colores' después de insertar los nuevos colores.
+print(colores)

c7ac797b4e867846f6428186ee76d16a2d9eb5e4
JonaBN <a22310188@ceti.mx>
2025-03-09 15:59:34 -0600

    Capitulo 10
    Se crea la lista colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'].
    Se imprimen los colores en posiciones negativas de la lista:
    
        colores[-1] es el último color, "naranja".
        colores[-7] es el cuarto color desde el final, "marrón".
        colores[-5] es el sexto color desde el final, "lila".
        colores[-2] es el segundo color desde el final, "blanco".
        colores[-10] es el primer color, "rojo".

---
+++ 010_Practica1_Posiciones_Negativas_Listas.py
@@ -0,0 +1,7 @@
+## JonathanBN
+#Posiciones negativas en listas Python
+
+# Se define una lista con colores.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+# Se imprimen los colores en posiciones negativas de la lista 'colores'.
+print(colores[-1],colores[-7],colores[-5],colores[-2],colores[-10])

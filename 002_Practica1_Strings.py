71420da153907bda5f1e913dd8f796b143e9231b
JonaBN <a22310188@ceti.mx>
2025-03-09 14:43:18 -0600

    Capitulo 2
    Se crean dos variables: var1 con comillas dobles y var2 con comillas simples, ambas contienen cadenas de texto.
    
    Luego, se almacena la frase literal "print()" se utiliza para imprimir valores en la consola. en la variable mensaje.
    
    Finalmente, se usa la función print() para imprimir el contenido de la variable mensaje.

---
+++ 002_Practica1_Strings.py
@@ -0,0 +1,14 @@
+## JonathanBN
+# ¿Qué son y cómo funcionan los strings en Python?
+
+# Se define una variable con comillas dobles que contiene un string
+var1=("Hello world ")
+
+# Se define una varille con comillas simples que contiene un string
+var2=('Hello world')
+
+# Almacena la frase literal en una variable.
+mensaje=('"print()" se utiliza para imprimir valores en la consola.')
+
+# Imprime el contenido de la variable mensaje.
+print(mensaje)

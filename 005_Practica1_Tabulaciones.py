0af9c85d4b2afa1b58647c171dbf30221fccb3cc
JonaBN <a22310188@ceti.mx>
2025-03-09 15:09:51 -0600

    Capitulo 5
    El código utiliza el carácter de escape \n para insertar saltos de línea y mostrar cada lenguaje de programación en una línea separada en la consola. La función print() se usa para imprimir la lista de lenguajes de programación.

---
+++ 005_Practica1_Tabulaciones.py
@@ -0,0 +1,5 @@
+##JonathanBN
+#Saltos de línea y tabulaciones en Python
+
+# Imprime una lista de lenguajes de programación, cada uno en una nueva línea.
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.\n-HTML.\n-Go.\n-C#")

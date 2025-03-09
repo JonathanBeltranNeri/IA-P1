d0bfdab88909ffef49e4464d494a09f89a9e0d6a
JonaBN <a22310188@ceti.mx>
2025-03-09 15:34:49 -0600

    Capitulo 8
    El código utiliza la función round() para redondear el número num a 5 decimales. Luego, imprime el resultado, que es 17.56738.

---
+++ 008_Practica1_Floats_Round.py
@@ -0,0 +1,6 @@
+## JonathanBN
+# Los floats y el método round() de Python
+
+# Redondea el número 'num' a 5 decimales y almacena el resultado.
+num= 17.567383292929200234
+print(round(num,5))# Imprime el número redondeado a 5 decimales.

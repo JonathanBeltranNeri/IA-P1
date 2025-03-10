5d627c4d1367c76c570f064e412e27622e102c9f
JonaBN <a22310188@ceti.mx>
2025-03-10 00:18:28 -0600

    Capitulo 16
    Se define la lista colores con una serie de nombres de colores.
    
    Se utiliza el método sort(reverse=True):
    
        sort() ordena los elementos de la lista alfabéticamente.
    
        reverse=True hace que el orden sea descendente (de la Z a la A).
    
    Se imprime la lista colores, mostrando los colores ordenados en orden inverso.

---
+++ 016_Practica1_Listas_Sort_Sorted.py
@@ -0,0 +1,12 @@
+## JonathanBN
+# Ordenamiento de elementos en listas Python
+
+# Se define una lista con colores.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón',
+'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Se ordena la lista en orden descendente usando el método sort() con reverse=True.
+colores.sort(reverse=True)
+
+# Se imprime la lista ordenada de forma descendente.
+print(colores)

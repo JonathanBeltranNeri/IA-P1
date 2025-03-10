f8387662ee6a272ae4480be55a6afbceaa44950f
JonaBN <a22310188@ceti.mx>
2025-03-10 10:27:33 -0600

    Capitulo 18
    Se define la tupla colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja').
    
    Se utiliza la indexación [] para acceder a un elemento específico de la tupla:
    
        colores[1] devuelve el segundo color de la tupla, que es "azul".
    
    Se define la tupla numeros = (10, 1, 5, 11), que almacena valores numéricos.
    
    Se realiza una operación matemática con los valores de la tupla numeros:
    
        numeros[0] + numeros[3] - numeros[1] + numeros[2]
        Suma 10 + 11, resta -1 y suma +5, dando el resultado 25.
    
    Se imprime el resultado de la operación utilizando print().

---
+++ 018_Practica1_Tuplas.py
@@ -0,0 +1,9 @@
+## JonathanBN
+# Cómo funcionan las tuplas en Python y las diferencias con las listas
+
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(colores[1])
+
+numeros = (10, 1, 5, 11)
+opereciones=numeros[0]+numeros[3]-numeros[1]+numeros[2]
+print(opereciones)

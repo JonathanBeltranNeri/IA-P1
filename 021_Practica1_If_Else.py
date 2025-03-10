df6ef0d4617e1c29369280ab2f0aa0ad0e306f96
JonaBN <a22310188@ceti.mx>
2025-03-10 10:46:18 -0600

    Capitulo 21
    La variable color se asigna el valor "rojo".
    
    Se utiliza una estructura if-else para verificar si el valor de color es igual a "rojo".
    
        Si la condición es verdadera (es decir, color == "rojo"), se ejecuta el bloque de código dentro del if y se imprime "El color es rojo".
    
        Si la condición fuera falsa (si color no fuera "rojo"), se ejecutaría el bloque dentro del else, y se imprimiría "El color no es rojo".

---
+++ 021_Practica1_If_Else.py
@@ -0,0 +1,13 @@
+## JonathanBN
+# El condicional IF ELSE de Python
+
+# Se asigna el valor "rojo" a la variable color
+color = "rojo"
+
+# Se evalúa si el valor de la variable color es igual a "rojo".
+# Como la condición es verdadera, se ejecuta el bloque de código dentro del if.
+if color == "rojo" :
+    print("El color es rojo")
+else :
+    # Si la condición fuera falsa, se ejecutaría este bloque.
+    print("El color no es rojo")

4981a7d12d2cbf8cf2f5a3c14d7266e9777979b5
JonaBN <a22310188@ceti.mx>
2025-03-10 11:19:12 -0600

    Capitulo 25
    Se definen las variables x y y con los valores 10000 y 200, respectivamente.
    Luego, se utiliza una expresión condicional ternaria para evaluar si x es menor que y.
    
        Si la condición x < y es verdadera, se imprime "x es menor que y".
    
        Si la condición es falsa (es decir, si x no es menor que y), se imprime "x no es menor que y".

---
+++ 025_Practica1_Tips_Condicionales.py
@@ -0,0 +1,8 @@
+## JonathanBN
+# Condicionales con sintaxis reducida Python
+
+x = 10000
+y = 200
+
+# Se utiliza una expresión condicional (ternaria) para verificar si x es menor que y
+print('x es menor que y.') if x < y else print('x no es menor que y')

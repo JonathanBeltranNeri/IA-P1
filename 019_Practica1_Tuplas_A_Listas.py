6a0d77a13839efaffacabff9338f6cfab2584651
JonaBN <a22310188@ceti.mx>
2025-03-10 10:35:18 -0600

    Capitulo 19
    Se define la lista colores, que contiene diferentes nombres de colores.
    
    Se utiliza la función tuple() para convertir la lista en una tupla:
    
        tupla = tuple(colores) convierte la lista colores en una tupla y la almacena en tupla.
    
    Se usa print(type(tupla)) para mostrar el tipo de dato de la variable tupla.
    
        Esto confirmará que la variable tupla ahora es del tipo tuple.

---
+++ 019_Practica1_Tuplas_A_Listas.py
@@ -0,0 +1,11 @@
+## JonathanaBN
+# Cómo convertir tuplas a listas y viceversa en PythoN
+
+# Se define una lista con diferentes nombres de colores.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Se convierte la lista en una tupla utilizando la función tuple().
+tupla = tuple(colores)
+
+# Se imprime el tipo de dato de la variable 'tupla' para verificar que es una tupla.
+print(type(tupla)) # Salida: <class 'tuple'>

e1237112d78a0093d4dfeeede2677c12f5d697d0
JonaBN <a22310188@ceti.mx>
2025-03-10 10:18:41 -0600

    Capitulo 17
    colores es una lista con 4 elementos: ['rojo', 'azul', 'verde', 'amarillo'].
    
    Se utiliza len() para saber cuantos elementos hay en una lista.
    
        len(colores) devuelve 4, porque la
    lista tiene cuatro elementos.
    
    print(longitud_colores) muestra ese número en la consola.

---
+++ 017_Practica1_Listas_Len.py
@@ -0,0 +1,7 @@
+## JonathanBN
+# Contar elementos de listas Python
+
+# Definimos la lista
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+longitud_colores = len(colores) # Contamos los elementos con len()
+print(longitud_colores)  # Imprimimos el resultado (4)

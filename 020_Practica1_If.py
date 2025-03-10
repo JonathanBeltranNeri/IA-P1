3827039c47fb69e192ed97bd0518153b10c71ec5
JonaBN <a22310188@ceti.mx>
2025-03-10 10:41:07 -0600

    Capitulo 20
    En el primer bloque, se compara si num1 (15) es diferente de num2 (20) usando el operador !=. Como los valores son diferentes, se ejecuta el bloque de código dentro del if.
    
    En el segundo bloque, se compara si num1 (1450) es mayor que num2 (60) usando el operador >. Como la condición es verdadera, se ejecuta el bloque de código dentro del if.
    
    En el tercer bloque, se compara si num1 (1450) es diferente de num2 (1450). Como son iguales, la condición es falsa y no se ejecuta el bloque de código dentro del if.

---
+++ 020_Practica1_If.py
@@ -0,0 +1,28 @@
+## JonathanBN
+# El condicional IF de Python y los operadores de comparación
+
+# Se asignan valores a las variables num1 y num2
+num1 = 15
+num2 = 20
+# Se evalúa si num1 es diferente a num2.
+# Como son diferentes, se ejecuta el bloque de código dentro del if.
+if num1 != num2:
+	print('Se ejecuta el if.')
+
+
+# Se cambian los valores de num1 y num2
+num1 = 1450
+num2 = 60
+# Se evalúa si num1 es mayor que num2.
+# Como 1450 es mayor que 60, se ejecuta el bloque de código dentro del if.
+if num1 > num2:
+	print('Se ejecuta el if.')
+	
+
+# Se cambian nuevamente los valores de num1 y num2
+num1 = 1450
+num2 = 1450
+# Se evalúa si num1 es diferente de num2.
+# Como num1 es igual a num2, el bloque de código dentro del if no se ejecuta
+if num1 != num2:
+	print('Se ejecuta el if.')

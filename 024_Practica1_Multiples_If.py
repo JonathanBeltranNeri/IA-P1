39774ca122772851c48999d59fef94710694c514
JonaBN <a22310188@ceti.mx>
2025-03-10 11:16:55 -0600

    Capitulo 24
    Se solicita al usuario que ingrese un número entre 1 y 4 usando input(). Este valor se convierte en un entero con int().
    
    Luego, se utilizan varias sentencias if para comparar el número ingresado con las opciones disponibles (1, 2, 3 y 4).
    
        Si el número ingresado es 1, se imprime "Has seleccionado la opción número 1".
    
        Si el número ingresado es 2, se imprime "Has seleccionado la opción número 2", y así sucesivamente para los números 3 y 4.

---
+++ 024_Practica1_Multiples_If.py
@@ -0,0 +1,15 @@
+## JonathanBN
+# Múltiples condiciones IF en Python
+
+# Se solicita al usuario que ingrese un número entre 1 y 4
+entrada = int(input('Introduce un número del 1 al 4:\n'))
+
+# Se verifica cuál opción ha seleccionado el usuario y se muestra el mensaje correspondiente
+if entrada == 1:
+    print('Has seleccionado la opción número 1.')
+if entrada == 2:
+    print('Has seleccionado la opción número 2.')
+if entrada == 3:
+    print('Has seleccionado la opción número 3.')
+if entrada == 4:
+    print('Has seleccionado la opción número 4.')

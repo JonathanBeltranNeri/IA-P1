a1a3af20e5015815a2d2aa42da1572a12e95dd98
JonaBN <a22310188@live.ceti.mx>
2025-03-11 22:08:44 -0600

    Capitulo 29
    Uso de range():
    
        Se usa range(7, 710, 100) para generar una secuencia de números. Comienza en 7, termina antes de 710 y aumenta en pasos de 100.
    
    Bucle for:
    
        El bucle for recorre cada valor generado por range() y asigna el valor a la variable x en cada iteración.
    
    Impresión:
    
        En cada iteración, se imprime el valor de x usando print().

---
+++ 029_Practica1_For_Range.py
@@ -0,0 +1,7 @@
+## JonathanBN
+# El bucle for de Python y la función range()
+
+# Se utiliza un bucle for con range para generar números desde 7 hasta 710, con un paso de 100
+for x in range(7, 710, 100):
+	# Se imprime el valor de x en cada iteración
+	print(x)

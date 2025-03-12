e3372af6b15784adc79fbabde44afb70eadc5b86
JonaBN <a22310188@live.ceti.mx>
2025-03-11 22:04:57 -0600

    Capitulo 28
    Tupla de colores:
    
        Se define una tupla llamada colores con los valores 'rojo', 'azul', 'verde', y 'amarillo'.
    
    Bucle for:
    
        El bucle for recorre cada elemento de la tupla colores. En cada iteración, el valor de cada color se asigna a la variable x.
    
    Impresión:
    
        En cada iteración, se imprime el mensaje "El color es:" seguido del valor de x (el color actual) y un punto al final.

---
+++ 028_Practica1_For.py
@@ -0,0 +1,12 @@
+## JonathanBN
+# El bucle for de Python con ejemplos fáciles
+
+# Se define una tupla llamada colores con varios valores de colores
+colores = ('rojo','azul','verde','amarillo')
+
+# Se utiliza un bucle for para iterar sobre cada elemento de la tupla colores
+for x in colores:
+    
+        # En cada iteración, se imprime el color actual
+    print("El color es:",x,".")
+    

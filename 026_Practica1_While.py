7fb96b14b2dc9e97b018ad13d8d34024773cd4dc
JonaBN <a22310188@live.ceti.mx>
2025-03-11 21:54:48 -0600

    Capitulo 26
    Primer bucle while:
    
        Comienza con x = 0.
        El bucle imprime el valor de x y luego lo incrementa de 5 en 5 hasta que x supere 15.
    
    Segundo bucle while:
    
        Después de finalizar el primer bucle, x es 20.
        El bucle imprime el valor de x y luego lo decrementa de 20 en 20 hasta que x sea menor que -100.
    
    Tercer bucle while:
    
        Se establece x = 10 al inicio.
        El bucle imprime "El valor del bucle es:" seguido del valor de x, y luego disminuye x en 1 hasta llegar a 0.

---
+++ 026_Practica1_While.py
@@ -0,0 +1,19 @@
+## JonathanBN
+# El bucle while de Python con ejemplos
+
+x=0
+# Primer bucle while que imprime números desde 0 hasta 15, incrementando de 5 en 5
+while x <= 15:
+	print(x)
+	x += 5 # Aumenta x en 5 en cada iteración
+	
+# Segundo bucle while que imprime números desde el valor de x hasta -100, disminuyendo de 20 en 20
+while x >= -100:
+	print(x)
+	x -=20# Disminuye x en 20 en cada iteración
+
+x=10
+# Tercer bucle while que imprime el valor de x desde 10 hasta 0, disminuyendo de 1 en 1
+while x >= 0:
+	print('El valor del bucle es:',x)
+	x -= 1# Disminuye x en 1 en cada iteración

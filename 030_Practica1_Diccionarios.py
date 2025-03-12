58d943dc1c93d64239c941f6aec2b7609d7735b3
JonaBN <a22310188@live.ceti.mx>
2025-03-11 22:14:03 -0600

    Capitulo 30
    Diccionarios teclado1 y teclado2:
    
        Se definen dos diccionarios, teclado1 y teclado2, que contienen información sobre dos teclados: su categoría, modelo y precio.
    
    Acceso a elementos del diccionario:
    
        El código accede a las claves 'Modelo' y 'Precio' del diccionario teclado2 para obtener los valores correspondientes.
    
    Impresión:
    
        Se imprime el modelo y el precio del teclado2 utilizando la función print(). El formato de salida será: "El modelo Corsair K55 RGB cuesta 59,99 $".

---
+++ 030_Practica1_Diccionarios.py
@@ -0,0 +1,19 @@
+## JonathanBN
+# ¿Qué son los diccionarios de Python?
+
+# Definición del primer diccionario 'teclado1' con información sobre un teclado
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+# Definición del segundo diccionario 'teclado2' con información sobre otro teclado
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+# Se imprime el modelo y precio del teclado2 utilizando las claves del diccionario
+print("El modelo",teclado2['Modelo'],"cuesta",teclado2['Precio'],"$")
